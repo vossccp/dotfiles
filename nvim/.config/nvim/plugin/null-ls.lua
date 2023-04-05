b5d685ee1fa6ef2f98fbd536559c6602ff6c844f
Carsten Voss <carsten.voss@gmx.de>
2023-04-05 08:42:55 +0200

    Format csharp code

--- nvim/.config/nvim/plugin/null-ls.lua
+++ nvim/.config/nvim/plugin/null-ls.lua
@@ -19,6 +19,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
     formatting.prettier,
+    formatting.csharpier,
     diagnostics.eslint_d.with({
       condition = function(utils)
         return utils.root_has_file({ '.eslintrc.js' })
