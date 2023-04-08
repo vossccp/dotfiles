e5dde36087de78423e5eb4351688ea6663c1d7dd
Carsten Voss <carsten.voss@gmx.de>
2023-04-08 11:08:46 +0200

    start with minimal setup

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -4,8 +4,7 @@ local lspconfig = require("mason-lspconfig")
 mason.setup()
 
 lspconfig.setup {
-  ensure_installed = { "lua_ls", "bashls", "cssls", "dockerls", "eslint", "gopls", "graphql", "html",
-    "kotlin_language_server", "ltex", "marksman" },
+  ensure_installed = { "lua_ls", "html", "kotlin_language_server", "marksman" },
   automatic_installation = {
     -- dont automatically install typescript server
     -- it will be installed via plugin Typescript and
