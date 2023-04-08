f3c69d3e54d51ab3ace7a97ed891c271306b8f9d
Carsten Voss <carsten.voss@gmx.de>
2023-04-08 11:08:33 +0200

    No eslint

--- nvim/.config/nvim/plugin/null-ls.lua
+++ nvim/.config/nvim/plugin/null-ls.lua
@@ -13,18 +13,12 @@ local lsp_formatting = function(bufnr)
 end
 
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
     formatting.prettier,
-    formatting.csharpier,
-    diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({ '.eslintrc.js' })
-      end
-    }),
+    formatting.csharpier
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
