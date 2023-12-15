743af60fdbef2dde9e8bab65fe897a033a96079e
Carsten Voss <carsten.voss@gmx.de>
2023-12-15 11:42:43 +0100

    First version of wezterm

---
+++ wezterm/.config/wezterm/utils/keys.lua
@@ -0,0 +1,35 @@
+local wt_action = require("wezterm").action
+local M = {}
+
+M.multiple_actions = function(keys)
+	local actions = {}
+	for key in keys:gmatch(".") do
+		table.insert(actions, wt_action.SendKey({ key = key }))
+	end
+	table.insert(actions, wt_action.SendKey({ key = "\n" }))
+	return wt_action.Multiple(actions)
+end
+
+M.key_table = function(mods, key, action)
+	return {
+		mods = mods,
+		key = key,
+		action = action,
+	}
+end
+
+M.cmd = function(key, action)
+	return M.key_table("CMD", key, action)
+end
+
+M.tmux = function(key, tmux_key)
+	return M.cmd(
+		key,
+		wt_action.Multiple({
+			wt_action.SendKey({ mods = "CTRL", key = "b" }),
+			wt_action.SendKey({ key = tmux_key }),
+		})
+	)
+end
+
+return M
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+local k = require("utils/keys")
+local act = wezterm.action
+
+local config = {
+	font = wezterm.font("JetBrains Mono"),
+	color_scheme = "Catppuccin Mocha",
+	font_size = 16.0,
+	window_padding = {
+		left = 8,
+		right = 8,
+		top = 8,
+		bottom = 8,
+	},
+	adjust_window_size_when_changing_font_size = false,
+	debug_key_events = false,
+	enable_tab_bar = false,
+	native_macos_fullscreen_mode = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	keys = {
+		k.cmd("e", act.SendKey({ mods = "CTRL", key = "e" })),
+		k.cmd("o", act.SendKey({ mods = "CTRL", key = "p" })),
+		k.tmux("t", "c"), -- tmux new-window
+		k.tmux("1", "1"),
+		k.tmux("2", "2"),
+		k.tmux("3", "3"),
+		k.tmux("4", "4"),
+		k.tmux("5", "5"),
+		k.tmux("6", "6"),
+		k.tmux("7", "7"),
+		k.tmux("8", "8"),
+		k.tmux("9", "9"),
+	},
+}
+
+return config
