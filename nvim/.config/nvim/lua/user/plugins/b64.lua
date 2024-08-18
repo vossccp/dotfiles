0e834ec5e4b47389e5067bbf626c5d20999491c2
Carsten Voss <carsten.voss@gmx.de>
2024-08-18 11:30:09 +0200

    base64 shortcutes

--- nvim/.config/nvim/lua/user/plugins/b64.lua
+++ nvim/.config/nvim/lua/user/plugins/b64.lua
@@ -2,7 +2,7 @@ return {
   'taybart/b64.nvim',
   config = function()
     local b64 = require('b64')
-    -- vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+
     vim.keymap.set('v', '<leader>eb', b64.encode, { desc = 'Base64: Encode' })
     vim.keymap.set('v', '<leader>ed', b64.decode, { desc = 'Base64: Decode' })
   end,
