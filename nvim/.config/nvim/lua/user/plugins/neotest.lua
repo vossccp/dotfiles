7384a093ee16152224becee373e45f61b5b5109a
Carsten Voss <carsten.voss@gmx.de>
2025-02-07 09:38:17 +0100

    setup neotest

--- nvim/.config/nvim/lua/user/plugins/neotest.lua
+++ nvim/.config/nvim/lua/user/plugins/neotest.lua
@@ -1,20 +1,26 @@
 return {
-  'nvim-neotest/neotest',
+  "nvim-neotest/neotest",
   dependencies = {
-    'Issafalcon/neotest-dotnet',
+    "Issafalcon/neotest-dotnet",
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
   },
   config = function()
-    require('neotest').setup {
+    local neotest = require("neotest")
+
+    neotest.setup({
       log_level = 1,
       adapters = {
-        require('neotest-dotnet')({
-          discovery_root = "project"
-        })
-      }
-    }
+        require("neotest-dotnet")({
+          discovery_root = "project",
+        }),
+      },
+    })
+
+    vim.keymap.set("n", "<leader>tr", "<cmd>Neotest run<CR>", { desc = "Run test" })
+    vim.keymap.set("n", "<leader>ta", "<cmd>Neotest run file<CR>", { desc = "Run all tests in file" })
+    vim.keymap.set("n", "<leader>to", "<cmd>Neotest output<CR>", { desc = "Open test output" })
   end,
 }
