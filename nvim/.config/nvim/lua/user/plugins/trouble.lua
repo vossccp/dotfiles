973d82fcf9c0c66e3e507aa3a2bb9dac274ec921
Carsten Voss <carsten.voss@gmx.de>
2025-02-07 09:37:32 +0100

    remove trouble (not used)

--- nvim/.config/nvim/lua/user/plugins/trouble.lua
+++ nvim/.config/nvim/lua/user/plugins/trouble.lua
@@ -7,11 +7,11 @@ return {
     -- refer to the configuration section below
   },
   cmd = { "Trouble" },
-  keys = {
-    { "<leader>tt", "<cmd>Trouble diagnostics toggle<CR>",    desc = "Open/close trouble list" },
-    { "<leader>tw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>td", "<cmd>Trouble document_diagnostics<CR>",  desc = "Open trouble document diagnostics" },
-    { "<leader>tq", "<cmd>Trouble quickfix<CR>",              desc = "Open trouble quickfix list" },
-    { "<leader>tl", "<cmd>Trouble loclist<CR>",               desc = "Open trouble location list" },
-  },
+  -- keys = {
+  --   { "<leader>tt", "<cmd>Trouble diagnostics toggle<CR>",    desc = "Open/close trouble list" },
+  --   { "<leader>tw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+  --   { "<leader>td", "<cmd>Trouble document_diagnostics<CR>",  desc = "Open trouble document diagnostics" },
+  --   { "<leader>tq", "<cmd>Trouble quickfix<CR>",              desc = "Open trouble quickfix list" },
+  --   { "<leader>tl", "<cmd>Trouble loclist<CR>",               desc = "Open trouble location list" },
+  -- },
 }
